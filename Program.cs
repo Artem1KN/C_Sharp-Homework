0a971addadf7f6e8a37d78866376789d8608d60a
Artem <91216403+Artem1KN@users.noreply.github.com>
2022-08-22 18:13:13 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppQueen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsCorrectMove("z1", "z1")) ;
+        }
+
+        public static bool IsCorrectMove(string from, string to)
+        {
+            var dx = Math.Abs(to[0] - from[0]); //смещение фигуры по горизонтали
+            var dy = Math.Abs(to[1] - from[1]); //смещение фигуры по вертикали
+
+            return (dx == dy || dx == 0 || dy == 0) && from != to;
+        }
+    }
+}
